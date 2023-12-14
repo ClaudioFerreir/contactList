dda6b1009e5df86717193ad1844eea649e02c1a4
Claudio Ferreira Junior <claudioferreirajr@hotmail.com>
2023-12-14 16:29:47 -0300

    contact model added

---
+++ src/models/contact.ts
@@ -0,0 +1,20 @@
+class Contact {
+  fullName: string
+  email: string
+  phone: number
+  id: number
+
+  constructor (
+    fullName: string,
+    email: string,
+    phone: number,
+    id: number
+  ) {
+    this.fullName = fullName
+    this.email = email
+    this.phone = phone
+    this.number = number
+  }
+}
+
+export default Contact
